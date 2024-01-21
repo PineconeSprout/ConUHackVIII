c2105683ee047a109be5b1fa182e6a83270ba5c3
yaro0 <yaroslava11@hotmail.com>
2024-01-21 10:01:01 -0500

    Added tree prefab

---
+++ ConUHackVIII/Assets/Tree.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+
+    [SerializeField] GameObject bloodParticles;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other != null && other.tag == "endWall")
+        {
+            Destroy(gameObject);
+        }
+        else if (other != null && other.tag == "player")
+        {
+            Instantiate(bloodParticles, transform.position, transform.rotation);
+            player.GetComponent<Soldier>().health--;
+        }
+    }
+}
